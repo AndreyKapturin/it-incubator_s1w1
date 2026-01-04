ebca4373b77ffee40e7fdbd4707e62598953af62
Andrey Kapturin <andruxa21097@gmail.com>
2026-01-04 13:56:20 +0700

    bulid: configure dev environment
    - Config typescript
    - Config jest
    - Config nodemon

---
+++ __tests__/simple.spec.ts
@@ -0,0 +1,5 @@
+describe('simple test', () => {
+    it('true to be true', () => {
+      expect(true).toBe(true)
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world!!!')
+})
+
+app.listen(3000 , () => console.log('App run on 3000 port'))
